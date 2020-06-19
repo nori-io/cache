7a8ec9a283dbbce5702bd9f8d52ac3ef1a5ca19c
bruteforce1414 <bruteforce1414@gmail.com>
2020-06-19 12:43:11 +0500

    feat: dependencies

---
+++ cache.go
@@ -0,0 +1,34 @@
+package cache
+
+import (
+	"time"
+
+	"github.com/nori-io/nori-common/v2/errors"
+	"github.com/nori-io/nori-common/v2/meta"
+	"github.com/nori-io/nori-common/v2/plugin"
+)
+
+const (
+	CacheInterface meta.Interface = "nori/cache/Cache@0.2.0"
+)
+
+type Cache interface {
+	Clear() error
+	Delete(key []byte) error
+	Get(key []byte) ([]byte, error)
+	Set(key []byte, value []byte, _ time.Duration) error
+}
+
+func GetCache(r plugin.Registry) (Cache, error) {
+	instance, err := r.Interface(CacheInterface)
+	if err != nil {
+		return nil, err
+	}
+	i, ok := instance.(Cache)
+	if !ok {
+		return nil, errors.InterfaceAssertError{
+			Interface: CacheInterface,
+		}
+	}
+	return i, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nori-io/cache
+
+go 1.14
+
+require github.com/nori-io/nori-common/v2 v2.0.2-0.20200524054818-63071143b833 // indirect
